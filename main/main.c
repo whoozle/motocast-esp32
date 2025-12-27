b81127c8b1f0548001083a2a9fac01f65a00c302
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2025-12-27 15:36:41 +0000

    add parse_uuid and use text uuids from uuidgen

--- main/main.c
+++ main/main.c
@@ -16,6 +16,55 @@ void waveshare_init(void);
 
 static const char *TAG = "main";
 
+esp_bt_uuid_t parse_uuid(const char *value) {
+    esp_bt_uuid_t uuid = {};
+    size_t length = strlen(value);
+    if (length == 4) {
+		uuid.len         = ESP_UUID_LEN_16;
+		uuid.uuid.uuid16 = 0;
+		for(int i=0;i < length;){
+			uint8_t MSB = value[i];
+			uint8_t LSB = value[i+1];
+
+			if(MSB > '9') MSB -= 7;
+			if(LSB > '9') LSB -= 7;
+			uuid.uuid.uuid16 += (((MSB&0x0F) <<4) | (LSB & 0x0F))<<(2-i)*4;
+			i+=2;
+		}
+	} else if (length == 8) {
+		uuid.len         = ESP_UUID_LEN_32;
+		uuid.uuid.uuid32 = 0;
+		for(int i=0;i<length;){
+			uint8_t MSB = value[i];
+			uint8_t LSB = value[i+1];
+
+			if(MSB > '9') MSB -= 7; 
+			if(LSB > '9') LSB -= 7;
+			uuid.uuid.uuid32 += (((MSB&0x0F) <<4) | (LSB & 0x0F))<<(6-i)*4;
+			i+=2;
+		}
+	} else if (length == 36) {
+		// If the length of the string is 36 bytes then we will assume it is a long hex string in
+		// UUID format.
+		uuid.len = ESP_UUID_LEN_128;
+		int n = 0;
+		for(int i=0;i<length;){
+			if(value[i] == '-')
+				i++;
+			uint8_t MSB = value[i];
+			uint8_t LSB = value[i+1];
+
+			if(MSB > '9') MSB -= 7;
+			if(LSB > '9') LSB -= 7;
+			uuid.uuid.uuid128[15-n++] = ((MSB&0x0F) <<4) | (LSB & 0x0F);
+			i+=2;
+		}
+	} else {
+		ESP_LOGE(TAG, "uuid length is not 2, 4 or 36 bytes");
+	}
+    return uuid;
+}
+
 static void gatts_motocast_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_if, esp_ble_gatts_cb_param_t *param);
 
 struct gatts_profile_inst {
@@ -43,49 +92,6 @@ static struct gatts_profile_inst gl_profile_tab[MOTOCAST_NUM_APPS] = {
 static uint8_t adv_config_done = 0;
 #define adv_config_flag      (1 << 0)
 #define scan_rsp_config_flag (1 << 1)
-
-static uint8_t adv_service_uuid128[32] = {
-    /* LSB <--------------------------------------------------------------------------------> MSB */
-    //first uuid, 16bit, [12],[13] is the value
-    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0xEE, 0x00, 0x00, 0x00,
-    //second uuid, 32bit, [12], [13], [14], [15] is the value
-    0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0xFF, 0x00, 0x00, 0x00,
-};
-
-// The length of adv data must be less than 31 bytes
-//static uint8_t test_manufacturer[TEST_MANUFACTURER_DATA_LEN] =  {0x12, 0x23, 0x45, 0x56};
-//adv data
-static esp_ble_adv_data_t adv_data = {
-    .set_scan_rsp = false,
-    .include_name = true,
-    .include_txpower = false,
-    .min_interval = 0x0006, //slave connection min interval, Time = min_interval * 1.25 msec
-    .max_interval = 0x0010, //slave connection max interval, Time = max_interval * 1.25 msec
-    .appearance = 0x00,
-    .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
-    .p_manufacturer_data =  NULL, //&test_manufacturer[0],
-    .service_data_len = 0,
-    .p_service_data = NULL,
-    .service_uuid_len = sizeof(adv_service_uuid128),
-    .p_service_uuid = adv_service_uuid128,
-    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
-};
-// scan response data
-static esp_ble_adv_data_t scan_rsp_data = {
-    .set_scan_rsp = true,
-    .include_name = true,
-    .include_txpower = true,
-    //.min_interval = 0x0006,
-    //.max_interval = 0x0010,
-    .appearance = 0x00,
-    .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
-    .p_manufacturer_data =  NULL, //&test_manufacturer[0],
-    .service_data_len = 0,
-    .p_service_data = NULL,
-    .service_uuid_len = sizeof(adv_service_uuid128),
-    .p_service_uuid = adv_service_uuid128,
-    .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
-};
 
 static esp_ble_adv_params_t adv_params = {
     .adv_int_min        = 0x20,
@@ -124,11 +130,9 @@ static void gatts_motocast_event_handler(esp_gatts_cb_event_t event, esp_gatt_if
     switch(event) {
         case ESP_GATTS_CREATE_EVT:
             {
-                static const uint8_t char_uuid[] = {0x80, 0x54, 0x6c, 0x7b, 0x7c, 0x9f, 0xd1, 0x67, 0x0, 0x75, 0x53, 0x59, 0x40, 0x4e, 0xb0, 0x87};
                 static uint8_t char_value[] = {'Y', 'E', 'S'};
                 profile->service_handle = param->create.service_handle;
-                profile->char_uuid.len = ESP_UUID_LEN_128;
-                memcpy(profile->char_uuid.uuid.uuid128, char_uuid, 16);
+                profile->char_uuid = parse_uuid("511f350c-5158-4853-81b8-4ab95a687168");
                 static esp_attr_value_t gatts_char_val =
                 {
                     .attr_max_len = 0x40,
@@ -158,12 +162,46 @@ static void ble_gatts_callback(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_i
             profile->gatts_if = gatts_if;
             profile->service_id.is_primary = true;
             profile->service_id.id.inst_id = 0;
-            profile->service_id.id.uuid.len = ESP_UUID_LEN_128;
-            static const uint8_t uuid[] = {0xd6, 0x42, 0xbc, 0xcc, 0x32, 0xa9, 0xc, 0xa1, 0x91, 0x7c, 0x90, 0x79, 0x91, 0xcb, 0x83, 0x7b};
-            memcpy(profile->service_id.id.uuid.uuid.uuid128, uuid, 16);
+            profile->service_id.id.uuid = parse_uuid("0b61c95e-f4e2-4080-9188-9d8ddb294925");
             ESP_ERROR_CHECK(esp_ble_gap_set_device_name("Motocast"));
 
             adv_config_done = adv_config_done | scan_rsp_config_flag;
+            // The length of adv data must be less than 31 bytes
+            //static uint8_t test_manufacturer[TEST_MANUFACTURER_DATA_LEN] =  {0x12, 0x23, 0x45, 0x56};
+            //adv data
+            static esp_ble_adv_data_t adv_data = {
+                .set_scan_rsp = false,
+                .include_name = true,
+                .include_txpower = false,
+                .min_interval = 0x0006, //slave connection min interval, Time = min_interval * 1.25 msec
+                .max_interval = 0x0010, //slave connection max interval, Time = max_interval * 1.25 msec
+                .appearance = 0x00,
+                .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
+                .p_manufacturer_data =  NULL, //&test_manufacturer[0],
+                .service_data_len = 0,
+                .p_service_data = NULL,
+                .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
+            };
+            adv_data.service_uuid_len = profile->service_id.id.uuid.len;
+            adv_data.p_service_uuid = profile->service_id.id.uuid.uuid.uuid128;
+            // scan response data
+            static esp_ble_adv_data_t scan_rsp_data = {
+                .set_scan_rsp = true,
+                .include_name = true,
+                .include_txpower = true,
+                //.min_interval = 0x0006,
+                //.max_interval = 0x0010,
+                .appearance = 0x00,
+                .manufacturer_len = 0, //TEST_MANUFACTURER_DATA_LEN,
+                .p_manufacturer_data =  NULL, //&test_manufacturer[0],
+                .service_data_len = 0,
+                .p_service_data = NULL,
+                .flag = (ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT),
+            };
+
+            scan_rsp_data.service_uuid_len = profile->service_id.id.uuid.len;
+            scan_rsp_data.p_service_uuid = profile->service_id.id.uuid.uuid.uuid128;
+
             ESP_ERROR_CHECK(esp_ble_gap_config_adv_data(&adv_data));
             ESP_ERROR_CHECK(esp_ble_gap_config_adv_data(&scan_rsp_data));
 
