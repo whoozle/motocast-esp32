a8b070fbce02aab1612cb9172689ebd82e0767d4
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2026-01-02 21:46:29 +0000

    remove noisy logs

--- main/src/video.c
+++ main/src/video.c
@@ -74,22 +74,18 @@ int video_packet_finished(struct video_packet *pkt) {
 #define MIN(a, b) ((a) < (b)? (a): (b))
 
 esp_h264_err_t video_decode(uint8_t *buffer, uint32_t buffer_len) {
-    ESP_LOGI(TAG, "processing %u bytes of decoded data", buffer_len);
     uint32_t src_offset = 0;
     while(src_offset < buffer_len) {
-        ESP_LOGI(TAG, "video_packet_process %u/%u", src_offset, buffer_len);
         src_offset += video_packet_process(&pkt, buffer + src_offset, buffer_len - src_offset);
         if (!video_packet_finished(&pkt))
             return ESP_H264_ERR_OK;
 
-        ESP_LOGI(TAG, "video_packet_process finished %u/%u", src_offset, buffer_len);
         esp_h264_dec_in_frame_t in_frame = {.raw_data = { pkt.data, pkt.data_len }};
         while (in_frame.raw_data.len)  {
             int ret = esp_h264_dec_process(h264_handle, &in_frame, &out_frame);
             if (ret != ESP_H264_ERR_OK) {
                 ESP_LOGI(TAG, "esp_h264_dec_process error: %d", ret);
             } else {
-                ESP_LOGI(TAG, "out frame: %p %u", out_frame.outbuf, out_frame.out_size);
                 if (out_frame.outbuf)
                     video_present_frame(out_frame.outbuf);
             }
@@ -98,7 +94,6 @@ esp_h264_err_t video_decode(uint8_t *buffer, uint32_t buffer_len) {
         }
         video_packet_free(&pkt);
     }
-    ESP_LOGI(TAG, "video_decode finished %u/%u", src_offset, buffer_len);
     return ESP_H264_ERR_OK;
 }
 
@@ -113,7 +108,6 @@ uint32_t video_packet_process(struct video_packet *pkt, const uint8_t *buffer, u
         if (pkt->header_read < 4)
             return src_offset;
         pkt->data_written = 0;
-        ESP_LOGI(TAG, "video_packet_process, new packet, data len: %u", pkt->data_len);
         pkt->data = (uint8_t*)malloc(pkt->data_len);
         if (!pkt->data) {
             ESP_LOGE(TAG, "malloc failed");
